458fa5eb7c1871939b4f2b9467afbb8be733b1b3
Jon Lastrella <lastrell@amazon.com>
2023-11-17 18:41:35 +0000

    [ServerlessClusterPolicy] Add schema

---
+++ aws-msk-serverlessclusterpolicy/src/main/java/software/amazon/msk/serverlessclusterpolicy/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.msk.serverlessclusterpolicy;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-msk-serverlessclusterpolicy.json");
+    }
+}
